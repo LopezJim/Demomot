733ed9fbbaad6899a242ee76b8a7d45d079e5812
Fabien <pittierfa.etml@gmail.com>
2017-06-14 08:10:33 +0200

    Typo3 12.06.17 v.FPR + STAGE
    (Après le commit précédent sont réapparus certains fichiers dans "unstaged files")

---
+++ typo3/typo3temp/var/Cache/Code/fluid_template/Standard_action_index_6988f2e23a726721b7a863c48db331a49e85621b.php
@@ -0,0 +1,107 @@
+<?php
+
+class Standard_action_index_6988f2e23a726721b7a863c48db331a49e85621b extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getLayoutName(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this; 
+return (string) 'main_layout';
+}
+public function hasLayout() {
+return TRUE;
+}
+public function addCompiledNamespaces(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$renderingContext->getViewHelperResolver()->addNamespaces(array (
+  'core' => 
+  array (
+    0 => 'TYPO3\\CMS\\Core\\ViewHelpers',
+  ),
+  'f' => 
+  array (
+    0 => 'TYPO3Fluid\\Fluid\\ViewHelpers',
+    1 => 'TYPO3\\CMS\\Fluid\\ViewHelpers',
+  ),
+  'formvh' => 
+  array (
+    0 => 'TYPO3\\CMS\\Form\\ViewHelpers',
+  ),
+));
+}
+
+/**
+ * section content
+ */
+public function section_040f06fd774092478d450774f5ba30c5da78acc8(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+$output0 = '';
+
+$output0 .= '
+<div id="content">
+    ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure2 = function() use ($renderingContext, $self) {
+$array3 = array (
+);return $renderingContext->getVariableProvider()->getByPath('content_main', $array3);
+};
+$arguments1 = array();
+$arguments1['value'] = NULL;
+
+$output0 .= isset($arguments1['value']) ? $arguments1['value'] : $renderChildrenClosure2();
+
+$output0 .= '
+</div>
+';
+
+return $output0;
+}
+/**
+ * Main Render function
+ */
+public function render(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+$output4 = '';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\LayoutViewHelper
+$renderChildrenClosure6 = function() use ($renderingContext, $self) {
+return NULL;
+};
+$arguments5 = array();
+$arguments5['name'] = NULL;
+$arguments5['name'] = 'main_layout';
+
+$output4 .= htmlspecialchars(TYPO3Fluid\Fluid\ViewHelpers\LayoutViewHelper::renderStatic($arguments5, $renderChildrenClosure6, $renderingContext), ENT_QUOTES);
+
+$output4 .= '
+';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\SectionViewHelper
+$renderChildrenClosure8 = function() use ($renderingContext, $self) {
+$output9 = '';
+
+$output9 .= '
+<div id="content">
+    ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure11 = function() use ($renderingContext, $self) {
+$array12 = array (
+);return $renderingContext->getVariableProvider()->getByPath('content_main', $array12);
+};
+$arguments10 = array();
+$arguments10['value'] = NULL;
+
+$output9 .= isset($arguments10['value']) ? $arguments10['value'] : $renderChildrenClosure11();
+
+$output9 .= '
+</div>
+';
+return $output9;
+};
+$arguments7 = array();
+$arguments7['name'] = NULL;
+$arguments7['name'] = 'content';
+
+$output4 .= '';
+
+return $output4;
+}
+
+
+}
+#2994620621    2847      
